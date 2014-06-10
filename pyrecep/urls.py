af96d491f2f886bf4c32637818d0e5db8b316f0d
Axel Díaz axelio <diaz.axelio@gmail.com>
2014-06-10 15:21:19 -0430

    Añadiendo url para agregar qbe

--- pyrecep/urls.py
+++ pyrecep/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^admin/consultas/', include('django_qbe.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$',  include(admin.site.urls)),
 )
